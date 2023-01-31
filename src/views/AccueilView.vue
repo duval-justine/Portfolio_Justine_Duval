b22bb7b6403e0f3d32b47fb7f94914dc93902c03
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-01-31 16:03:05 +0100

    Réalisation du fond de ma page d'accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,5 +1,7 @@
 <template>
+  <div class="container h-[1024px] w-screen bg-gradient-to-b from-pastelbleu to-pastelrose">
 
+  </div>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         "Extended/red/50": "#FEF2F2",
         "Extended/red/900": "#F87171",
         "Extended/true-gray/900": "#171717",
+        "pastelbleu": "#879CC9",
+        "pastelrose": "#D5B3B2",
+
       },
       "fontFamily": {
         "space-grotesk": "Space Grotesk",
