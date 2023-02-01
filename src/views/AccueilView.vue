2a09ed1f96adc8ea2fc4140aac03e2923d1616ce
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-01 18:53:15 +0100

    import du 2e bloc dans la page d'accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,33 +1,37 @@
 <template>
-  <div class="container z-0 bg-gradient-to-b from-pastelbleu to-pastelrose">
-    <Header class="relative z-10" />
-    <div class="flex flex-row gap-36 items-center">
-      <div class="w-[750px] flex flex-col gap-4 ">
-        <h1 class="font-space-grotesk font-medium text-6xl text-Extended/red/50 ml-20">Justine Duval</h1>
-        <p class="font-space-grotesk font-light text-2xl text-Extended/red/50 ml-20">Étudiante en BUT MMI à Montbéliard
-          et spécialisée dans le design pour devenir Infographiste ou Web-Designeuse.</p>
+  <main>
+    <div class="container z-0 bg-gradient-to-b from-pastelbleu to-pastelrose">
+      <Header class="relative z-10" />
+      <div class="flex flex-row gap-36 items-center">
+        <div class="w-[750px] flex flex-col gap-4 ">
+          <h1 class="font-space-grotesk font-medium text-6xl text-Extended/red/50 ml-20">Justine Duval</h1>
+          <p class="font-space-grotesk font-light text-2xl text-Extended/red/50 ml-20">Étudiante en BUT MMI à
+            Montbéliard
+            et spécialisée dans le design pour devenir Infographiste ou Web-Designeuse.</p>
+        </div>
+        <MongolfiereNuages id="mongolfiere" class="float-right mr-10 z-20 w-full h-full object-contain relative " />
       </div>
-      <MongolfiereNuages id="mongolfiere" class="float-right mr-10 z-20 w-full h-full object-contain relative " />
+      <div class="flex flex-col items-center gap-4">
+        <RouterLink class="font-space-grotesk font-bold text-sm text-Extended/red/50 lg:text-lg lg:" to="/">
+          SCROLL
+        </RouterLink>
+        <div class="border-t-4 w-8 rotate-90 border-Extended/red/50"></div>
+      </div>
     </div>
-    <div class="flex flex-col items-center gap-4">
-      <RouterLink class="font-space-grotesk font-bold text-sm text-Extended/red/50 lg:text-lg lg:" to="/">
-        SCROLL
-      </RouterLink>
-      <div class="border-t-4 w-8 rotate-90 border-Extended/red/50"></div>
+    <div>
+      <Bloc1 class="pt-48" />
+      <Bloc2 />
     </div>
-  </div>
-  <div>
-    <Bloc1 class="pt-48" />
-  </div>
-  <div id="hot-air-balloon"></div>
+  </main>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
 import Bloc1 from "../components/Bloc1.vue";
+import Bloc2 from "../components/Bloc2.vue";
 import MongolfiereNuages from "../components/MongolfiereNuages.vue";
 export default {
-  components: { Header, MongolfiereNuages, Bloc1 }
+  components: { Header, MongolfiereNuages, Bloc1, Bloc2 }
 }
 </script>
 
@@ -61,5 +65,10 @@ export default {
     top: 0px;
     left: 0px;
   }
+}
+
+.container {
+  width: 100%;
+  max-width: none;
 }
 </style>
