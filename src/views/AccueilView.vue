a7042c7ec455b33071629bb36d4320d0fb6b04fd
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-01 17:42:10 +0100

    Ajout du bloc  dans la page d'accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container z-0 h-[1024px] w-screen bg-gradient-to-b from-pastelbleu to-pastelrose">
+  <div class="container z-0 bg-gradient-to-b from-pastelbleu to-pastelrose">
     <Header class="relative z-10" />
     <div class="flex flex-row gap-36 items-center">
       <div class="w-[750px] flex flex-col gap-4 ">
@@ -10,17 +10,22 @@
       <MongolfiereNuages class="float-right mr-10 z-20 max-width: 100% h-auto" />
     </div>
     <div class="flex flex-col items-center gap-4">
-      <RouterLink class="font-space-grotesk font-bold text-sm text-Extended/red/50 lg:text-lg lg:" to="/">SCROLL
+      <RouterLink class="font-space-grotesk font-bold text-sm text-Extended/red/50 lg:text-lg lg:" to="/">
+        SCROLL
       </RouterLink>
       <div class="border-t-4 w-8 rotate-90 border-Extended/red/50"></div>
     </div>
+  </div>
+  <div>
+    <Bloc1 class="pt-48" />
   </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
+import Bloc1 from "../components/Bloc1.vue";
 import MongolfiereNuages from "../components/MongolfiereNuages.vue";
 export default {
-  components: { Header, MongolfiereNuages }
+  components: { Header, MongolfiereNuages, Bloc1 }
 }
 </script>
