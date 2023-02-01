12aba2ed75b5ff233896905efe40afccc3e3db6e
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-01 10:46:33 +0100

    Ajout du SVG de la mongolfiere avec les nuages

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="container z-0 h-[1024px] w-screen bg-gradient-to-b from-pastelbleu to-pastelrose">
     <Header class="relative z-10" />
-    <Mongolfiere class="float-right mr-10" />
+    <div class="">
+      <MongolfiereNuages class="float-right mr-10 z-20" />
+    </div>
+
   </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
-import Mongolfiere from "../components/Mongolfiere.vue";
+import MongolfiereNuages from "../components/MongolfiereNuages.vue";
 export default {
-  components: { Header, Mongolfiere }
+  components: { Header, MongolfiereNuages }
 }
 </script>
