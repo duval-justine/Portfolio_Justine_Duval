fffcda95db245062797219f3a355dffd2c4ec4b5
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-01 17:41:58 +0100

    Réalisation d'un premier bloc

---
+++ src/components/Bloc1.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex flex-row py-32 px-64">
+        <div>
+            <div class="container1 overflow-hidden mx-auto shadow-2xl">
+                <img src="../../public/images/Janvier2023_Fond.jpg" class="max-width: 100% h-auto"
+                    alt="Mot Janvier sur un fond bleu">
+            </div>
+        </div>
+        <div>
+            <div class="flex flex-col gap-1 mt-20">
+                <h3 class="font-space-grotesk text-2xl text-Extended/true-gray/900 font-medium ml-3">JANVIER</h3>
+                <div class="border-t-4 w-64  border-Extended/true-gray/900"></div>
+                <p class="font-space-grotesk text-sm text-Extended/true-gray/900 font-light ml-3 ">Illustrations &
+                    Motions
+                    Design
+                    |
+                    2023</p>
+            </div>
+            <div class="container2 overflow-hidden mx-auto shadow-2xl -ml-28 mt-20">
+                <img src="../../public/images/Janvier2023_Ski.png" class="w-full" alt="Mot Janvier sur un fond bleu">
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style>
+.container1 {
+    width: 650px;
+    height: 450px;
+}
+
+.container2 {
+    width: 382px;
+    height: 382px;
+}
+</style>
