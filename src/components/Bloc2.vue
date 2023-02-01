2cf5ab8970ded565a256bc124ee54cddb6bb4f5c
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-01 18:53:02 +0100

    création du 2e bloc

---
+++ src/components/Bloc2.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="flex flex-row py-32 px-64">
+        <div>
+            <div class="relative container2 overflow-hidden mx-auto shadow-2xl mt-20 z-30">
+                <img src="../../public/images/CRBX_Logo.jpg" class="w-full" alt="Mot Janvier sur un fond bleu">
+            </div>
+            <div class="flex flex-col gap-1 mt-8 items-end mr-32">
+                <h3 class="font-space-grotesk text-2xl text-Extended/true-gray/900 font-medium mr-3">CREATE BOX</h3>
+                <div class="border-t-4 w-48  border-Extended/true-gray/900"></div>
+                <p class="font-space-grotesk text-sm text-Extended/true-gray/900 font-light mr-3 ">Générateur de NFT |
+                    2023</p>
+            </div>
+        </div>
+        <div>
+            <div class="container3 overflow-hidden mx-auto shadow-2xl z-10 -ml-32">
+                <img src="../../public/images//CRBX_Carte.jpg" class="max-width: 100% h-auto"
+                    alt="Mot Janvier sur un fond bleu">
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style>
+.container2 {
+    width: 382px;
+    height: 382px;
+}
+
+.container3 {
+    width: 650px;
+    height: 595px;
+}
+</style>
