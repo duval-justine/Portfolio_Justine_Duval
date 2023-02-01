e862f8485ce8854ab4fd5627fd755edd6edf6206
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-01 10:33:23 +0100

    Ajout de la mongolfière sur la page d'accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container z-0 h-[1024px] w-screen bg-gradient-to-b from-pastelbleu to-pastelrose">
     <Header class="relative z-10" />
+    <Mongolfiere class="float-right mr-10" />
   </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
+import Mongolfiere from "../components/Mongolfiere.vue";
 export default {
-  components: { Header }
+  components: { Header, Mongolfiere }
 }
 </script>
