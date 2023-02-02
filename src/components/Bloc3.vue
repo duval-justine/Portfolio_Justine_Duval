66b4df112debc9600b9e465aa1c4f53c38cca93a
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-02 19:35:26 +0100

    Création du 3e bloc pour la page d'accueil

---
+++ src/components/Bloc3.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex flex-row py-32 px-64">
+        <div>
+            <div class="relative z-30 container4 overflow-hidden mx-auto shadow-2xl">
+                <img src="../../public/images/Citadelle.jpg" class="max-width: 100% h-auto"
+                    alt="Illustration de la Citadelle de Besançon de nuit">
+            </div>
+            <div class="flex flex-col gap-1 mt-9 items-end mr-14">
+                <h3 class="font-space-grotesk text-2xl text-Extended/true-gray/900 font-medium mr-3">LA CITADELLE DE
+                    BESANÇON</h3>
+                <div class="border-t-4 w-[330px]  border-Extended/true-gray/900"></div>
+                <p class="font-space-grotesk text-sm text-Extended/true-gray/900 font-light mr-3 ">Illustration animée |
+                    2022</p>
+            </div>
+        </div>
+        <div>
+            <div class="container2 overflow-hidden mx-auto shadow-2xl -ml-14 mt-40">
+                <img src="../../public/images/Citadelle_Moodboard.jpg" class="w-full"
+                    alt="MoodBoard pour mon illustration">
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style>
+.container4 {
+    width: 650px;
+    height: 418px;
+}
+
+.container2 {
+    width: 382px;
+    height: 382px;
+}
+</style>
