d33dfe29a868a70a4a4d549342bf6f4997fdc4dc
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-03 10:50:27 +0100

    TopPage Janvier 2023

---
+++ src/views/Janvier2023View.vue
@@ -0,0 +1,10 @@
+<template>
+    <TopPage img_test="/images/Janvier2023/Janvier2023_Fond.svg" />
+</template>
+
+<script>
+import TopPage from "../components/TopPage.vue";
+export default {
+    components: { TopPage }
+}
+</script>
