661ae083d2300e8a9f62a1ee320904b24ace2c57
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-03 09:50:34 +0100

    Ajout du bouton scroll dans le composant

--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -4,7 +4,15 @@
             <img :src="img_test" class="container0 h-screen z-0 w-full object-cover absolute" alt="">
             <!-- Header -->
             <Header class="relative z-10 " />
-
+            <!-- Bouton scroll -->
+            <div class="flex flex-col items-center  gap-4">
+                <RouterLink
+                    class="font-space-grotesk font-bold text-sm absolute bottom-6 text-Extended/red/50 lg:text-lg lg:"
+                    to="/">
+                    SCROLL
+                </RouterLink>
+                <div class=" border-t-4 w-8 rotate-90 absolute bottom-0 border-Extended/red/50"></div>
+            </div>
         </div>
 
     </section>
