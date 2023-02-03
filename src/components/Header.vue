48a67d0c055b8d197af8f6dde1da2f749a804070
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-03 13:19:35 +0100

    Animation Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,9 @@
         <RouterLink to="/">
             <Logo class="relative z-30 ml-8" />
         </RouterLink>
-        <div class=" mx-7
+        <div>
+            <nav>
+                <ul class=" mx-7
         relative
         flex flex-shrink-0 flex-grow-0
         items-center
@@ -14,9 +16,14 @@
         font-bold
         text-white pr-5
       ">
-            <RouterLink class="lg:text-lg lg:" to="/">Projets</RouterLink>
-            <RouterLink class="lg:text-lg lg:" to="/">À Propos</RouterLink>
-
+                    <li>
+                        <RouterLink class="anim lg:text-lg " to="/">Projets</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink class=" anim lg:text-lg lg:" to="/">À Propos</RouterLink>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
 </template>
@@ -26,3 +33,25 @@ export default {
     components: { Logo }
 }
 </script>
+
+<style>
+nav ul li {
+    position: relative;
+}
+
+nav ul li a::after {
+    content: "";
+    width: 0;
+    height: 3px;
+    position: absolute;
+    bottom: -2px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: #F87171;
+    transition: width 0.3s;
+}
+
+nav ul li a:hover::after {
+    width: 50%;
+}
+</style>
