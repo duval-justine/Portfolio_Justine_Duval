48d6ffbc4f5c4d6b1c644d992f76bda7469685dc
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-03 09:11:00 +0100

    Ajout d'une couleur

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         "Extended/true-gray/900": "#171717",
         "pastelbleu": "#879CC9",
         "pastelrose": "#D5B3B2",
-
+        "brun": "#5B3B3B",
       },
       "fontFamily": {
         "space-grotesk": "Space Grotesk",
