7b700075b75366381207529a3cbf92734897738e
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-03 12:42:43 +0100

    Modif bouton scroll "absolute"

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -25,11 +25,11 @@
           </div>
           <!-- Bouton scroll -->
           <div class="flex flex-col items-center  gap-4">
-            <RouterLink
-              class="font-space-grotesk font-bold text-sm absolute bottom-6 text-Extended/red/50 lg:text-lg lg:" to="/">
+            <RouterLink class="font-space-grotesk font-bold text-sm  bottom-6 text-Extended/red/50 lg:text-lg lg:"
+              to="/">
               SCROLL
             </RouterLink>
-            <div class=" border-t-4 w-8 rotate-90 absolute bottom-0 border-Extended/red/50"></div>
+            <div class=" border-t-4 w-8 rotate-90 bottom-0 border-Extended/red/50"></div>
           </div>
         </div>
       </section>
