51dbf680b424b4cdb071502a68af669c869197df
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-03 09:48:42 +0100

    Création d'un composant pour le top page

---
+++ src/components/TopPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <div class="container0 ">
+            <img :src="img_test" class="container0 h-screen z-0 w-full object-cover absolute" alt="">
+            <!-- Header -->
+            <Header class="relative z-10 " />
+
+        </div>
+
+    </section>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+export default {
+    components: { Header },
+    props: {
+        img_test: String,
+    },
+}
+</script>
