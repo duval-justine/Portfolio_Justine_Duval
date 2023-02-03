92a2661441ff344f9cd827439208bebab79c4cff
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-03 10:52:01 +0100

    Création du chemin pour la page Janvier 2023

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '../views/AccueilView.vue'
 import CitadelleView from '../views/CitadelleView.vue'
+import Janvier2023View from '../views/CitaJanvier2023.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'Accueil', component: AccueilView },
     { path: '/citadelle', name: 'Citadelle', component: CitadelleView },
+    { path: '/janvier2023', name: 'Janvier2023', component: Janvier2023View },
 
   ]
 })
