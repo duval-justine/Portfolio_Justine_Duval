c4a58c8ce70f8ef737e5129d064f0a638889a5b3
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-03 09:49:27 +0100

    Création du chemoin de la page citadelle

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '../views/AccueilView.vue'
+import CitadelleView from '../views/CitadelleView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'Accueil', component: AccueilView },
-    // ici les autre routes
+    { path: '/citadelle', name: 'Citadelle', component: CitadelleView },
+
   ]
 })
 
