2d23cd715f3489084e6e781e4d214c0841bd005d
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-03 09:49:15 +0100

    Création de la page Citadelle avec componant

---
+++ src/views/CitadelleView.vue
@@ -0,0 +1,10 @@
+<template>
+    <TopPage img_test="/images/Citadelle.jpg" />
+</template>
+
+<script>
+import TopPage from "../components/TopPage.vue";
+export default {
+    components: { TopPage }
+}
+</script>
