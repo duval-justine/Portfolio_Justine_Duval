d7f2ce0d8ecb8d7ec35054bb6c800eadf21e1505
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-21 14:43:13 +0100

    Importation du Top Page dans Charte Bavans

---
+++ src/views/CharteBavansView.vue
@@ -0,0 +1,10 @@
+<template>
+    <TopPage img_test="/images/Bavans/Bavans_Couverture.svg" />
+</template>
+
+<script>
+import TopPage from "../components/TopPage.vue";
+export default {
+    components: { TopPage }
+}
+</script>
