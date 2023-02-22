cbd0f163833cb1a8fad3d65d31948b72e1bf42ea
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-22 22:43:18 +0100

    Début de la mise en page pour la charte Bavans

--- src/views/CharteBavansView.vue
+++ src/views/CharteBavansView.vue
@@ -1,5 +1,147 @@
 <template>
-    <TopPage img_test="/images/Bavans/Bavans_Couverture.svg" />
+    <main>
+        <section>
+            <TopPage img_test="/images/Bavans/Bavans_CouverturePage.webp" class="h-[100vh]" />
+        </section>
+        <section>
+            <div>
+                <h1 class="font-space-grotesk font-bold text-lg mt-14 ml-24 text-Extended/true-gray/900">
+                    Refonte
+                    de la charte
+                    graphique de
+                    Bavans -
+                    Décembre 2022</h1>
+                <p class="font-space-grotesk font-light text-base mx-24 mt-5 text-justify mb-[523px]">La commune de
+                    Bavans se
+                    trouve
+                    dans
+                    le département
+                    du Doubs en Franche-Comté. Bavans est une commune
+                    dynamique et accueillante qui est bordée par la rivière Le Doubs qui lui offre une vue
+                    magnifique sur
+                    les
+                    paysages alentours. On peut y trouver de nombreux commerces et services. La commune est entourée
+                    de
+                    verdure
+                    et de nombreux sentiers de randonnées sont disponibles et offrent des activités variées pour les
+                    petits
+                    et
+                    grands. </p>
+            </div>
+            <div class="flex col gap-14 mx-24 mb-11">
+                <div>
+                    <h3 class="font-space-grotesk font-medium text-lg mt-14  text-Extended/true-gray/900">MoodBoard
+                    </h3>
+                    <p class="font-space-grotesk font-light w-[700px] text-base  mt-5 text-justify">Pour ce projet, nous
+                        devions
+                        choisir
+                        un site institutionnel et proposer une refonte de la charte graphique pour celui-ci. Pour ma part
+                        j’ai
+                        choisi le site de Bavans, qui est un site très peu mis à jour qui comporte de nombreux problèmes
+                        notamment
+                        au niveau du responsive et de la fluidité et de la navigation sur les différentes pages.
+                    </p>
+                    <p class="font-space-grotesk font-light text-base w-[700px]  mt-5 text-justify ">Pour la
+                        refonte de
+                        cette charte
+                        graphique, j’ai donc réalisé plusieurs analyses sur la ville notamment sur les couleurs déjà
+                        présentes
+                        sur
+                        l’identité visuelle ainsi que les monuments importants et les différents espaces de la ville. Pour
+                        pouvoir
+                        regrouper toutes mes analyses, j’ai donc réalisé ce Moodboard ce qui m’a permis de rester sur le
+                        thème
+                        de
+                        base et de ne pas m’en défaire.
+                    </p>
+                </div>
+                <div class="w-full object-cover ">
+                    <img src="../../public/images/Bavans/Bavans_Moodboard.webp"
+                        alt="MoodBoard avec des images de la ville, des couleurs et des typographies">
+                </div>
+            </div>
+            <div>
+                <h3 class="font-space-grotesk font-medium text-lg mt-14 ml-24  text-Extended/true-gray/900">Identité
+                    Visuelle
+                </h3>
+                <p class="font-space-grotesk font-light text-base mx-24 mt-5 text-justify mb-[523px]">Ensuite, j’ai réalisé
+                    la refonte
+                    de l’identité visuelle de Bavans. J’ai donc commencé par analyser la précédente où j’ai constaté que le
+                    graphiste a mis en avant la courbe du Doubs qui longe la ville. J’ai donc voulu garder cette courbure et
+                    mettre en avant un monument emblématique de la ville. Pour cela, j’ai réalisé plusieurs croquis et
+                    ensuite je les ai vectorisés pour en arriver au résultat final.
+                </p>
+            </div>
+            <div>
+                <p class="font-space-grotesk font-light text-base mx-24 mt-5 text-justify mb-[523px]">Voici ma nouvelle
+                    proposition d’identité visuelle de Bavans où nous pouvons retrouver la courbure du Doubs dans la lettre
+                    B ainsi qu’un monument emblématique qui se trouve au centre de la ville, la fontaine de Bavans.
+                </p>
+            </div>
+            <div>
+                <div class="flex col gap-14 mx-24 mb-11 items-start">
+                    <div>
+                        <h3 class="font-space-grotesk font-medium text-lg mt-5 ml-24  text-Extended/true-gray/900">Harmonie
+                            Colorée
+                        </h3>
+                        <p class="font-space-grotesk font-light text-base mx-24 mt-5 text-justify mb-[523px]">Par la suite,
+                            je
+                            me
+                            suis attardée à l’harmonie colorée, j’ai choisi de repartir dans les mêmes nuances de couleurs
+                            qui
+                            sont
+                            déjà présentes sur le site et sur l’identité visuelle pour rester fidèle à la ville. Pour ne pas
+                            apporter trop de couleur au Logo, j’ai décidé que chaques couleurs représentent une catégories
+                            notamment
+                            le vert pour les causes environnementales, le bleu pour les causes institutionnelles et le rouge
+                            pour
+                            les causes de solidarité et le sport.
+                        </p>
+                    </div>
+                    <div class="w-[2500px] object-cover">
+                        <img src="../../public/images/Bavans/Bavans_HarmonieColoree.webp"
+                            alt="Les différentes couleurs pour la charte graphique">
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h3 class="font-space-grotesk font-medium text-lg mt-14 ml-24  text-Extended/true-gray/900">Goodies
+                </h3>
+                <p class="font-space-grotesk font-light text-base mx-24 mt-5 text-justify mb-[523px]">Pour pouvoir réaliser
+                    ce projet avec cette nouvelle charte graphique, j’ai réalisé plusieurs mockup notamment pour la
+                    créations de Goodies
+                </p>
+            </div>
+            <div>
+                <h3 class="font-space-grotesk font-medium text-lg mt-14 ml-24  text-Extended/true-gray/900">Site Web
+                </h3>
+                <p class="font-space-grotesk font-light text-base mx-24 mt-5 text-justify mb-9">Également une idée de
+                    refonte pour le site principale avec un avant et après. En mettant en avant les différentes rubriques
+                    correspondantes aux couleurs vues précédemment ainsi qu’en mettant les informations essentielles pour
+                    permettre aux utilisateurs de les retrouver plus facilement.
+                </p>
+                <div class="flex col gap-10 items-center align-middle mx-24 object-cover w-[600px]">
+                    <img src="../../public/images/Bavans/Bavans_WebAvant.webp" alt="">
+                    <img src="../../public/images/Bavans/Bavans_WebApres.webp" alt="">
+                </div>
+            </div>
+            <div>
+                <h3 class="font-space-grotesk font-medium text-lg mt-14 ml-24  text-Extended/true-gray/900">Journal
+                    Bimestriel d'information
+                </h3>
+                <p class="font-space-grotesk font-light text-base mx-24 mt-5 text-justify mb-9 ">Et pour terminer,
+                    j’ai
+                    aussi réalisé une proposition pour le journal bimestriel d’information avec une nouvelle refonte en
+                    reprenant celui de Novembre Décembre 2022. J’ai donc repris tous les textes à l'identique et j’ai
+                    apporté beaucoup plus d’espace en supprimant certains cadres et en structurant mieux les textes.
+                </p>
+                <div class="flex col gap-10 justify-center h-[600px]">
+                    <img src="../../public/images/Bavans/Bavans-InfoAvant.webp" alt="">
+                    <img src="../../public/images/Bavans/Bavans-InfoApres.webp" alt="">
+                </div>
+            </div>
+        </section>
+    </main>
 </template>
 
 <script>
@@ -8,3 +150,5 @@ export default {
     components: { TopPage }
 }
 </script>
+
+<style></style>
