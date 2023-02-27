1dffc07cdfbaf1d49ab615f0091b4de04e5cd418
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-02-27 14:20:23 +0100

    Début de la mise en page de la page projet sco

---
+++ src/views/ProjetScoView.vue
@@ -0,0 +1,14 @@
+<template>
+    <main>
+        <section>
+            <TopPage img_test="" class="h-[100vh] bg-zinc-500" />
+        </section>
+    </main>
+</template>
+
+<script>
+import TopPage from "../components/TopPage.vue";
+export default {
+    components: { TopPage }
+}
+</script>
