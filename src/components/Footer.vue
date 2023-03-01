255cb4b02993e7d5dc92054b1a984ae8b77f5f7c
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-01 19:50:44 +0100

    Mise en page du footer sans icon réseaux

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,26 @@
 <template>
-    <RouterLink to="/">
-        <Logo class="relative z-30 ml-8" />
-    </RouterLink>
+    <div class="bg-Extended/true-gray/900 flex flex-row justify-between items-center py-10 px-16">
+        <div class="flex flex-row items-center font-karla font-normal text-lg text-Extended/red/50 gap-4">
+            <RouterLink to="/">
+                <Logo class="z-30 ml-8" />
+            </RouterLink>
+            <h4>Justine Duval</h4>
+        </div>
+        <div class="font-space-grotesk font-medium text-lg justify-center text-Extended/red/50 flex flex-row gap-16">
+            <RouterLink to="/">
+                Projets
+            </RouterLink>
+            <RouterLink to="/">
+                Mentions légales
+            </RouterLink>
+            <RouterLink to="/">
+                À propos
+            </RouterLink>
+        </div>
+        <div>
+
+        </div>
+    </div>
 </template>
 <script>
 import Logo from "./icons/IconLogo.vue";
