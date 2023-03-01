0ca30b66362d0a03c05033e50c12e359c38937ab
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-01 13:25:57 +0100

    Body -> Main

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-
-  <body class="bg-Extended/red/50">
-  </body>
+  <main class="bg-Extended/red/50">
+  </main>
   <RouterView />
 </template>
 
 <script>
-
+import Logo from "../src/components/icons/IconLogo.vue";
+export default {
+  components: { Logo }
+}
 </script>
+<style></style>
