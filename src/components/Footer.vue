2e0fd5830e0039242188810900ef6be91b04937d
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-01 13:26:54 +0100

    Création du component Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <RouterLink to="/">
+        <Logo class="relative z-30 ml-8" />
+    </RouterLink>
+</template>
+<script>
+import Logo from "./icons/IconLogo.vue";
+export default {
+    components: { Logo }
+}
+</script>
+
+<style></style>
