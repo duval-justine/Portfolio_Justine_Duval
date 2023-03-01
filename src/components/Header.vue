d17bdf6b37aa97d7787398bdb9965b7d2e73b85c
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-01 20:55:31 +0100

    Début du menu responsive forat 3xs

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,31 @@
 <template>
+    <!-- Menu Format 3XS -->
+    <div class="xs:hidden z-50">
+        <input type="checkbox" id="active">
+        <label for="active" class="menu-btn"><i class="fas fa-bars"></i></label>
+        <div class="wrapper">
+            <ul class="font-space-grotesk text-[60px] font-bold   text-white">
+                <li>
+                    <RouterLink to="/projetsco">Projets</RouterLink>
+                </li>
+                <li>
+                    <RouterLink class="lg:text-lg lg:" to="/">À Propos</RouterLink>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- Fin du Menu Format 3XS -->
+
+
+    <!-- Menu Format MD -->
     <div class="relative z-30 mx-10 hidden justify-between pt-7 md:flex">
         <RouterLink to="/">
             <Logo class="relative z-30 ml-8" />
         </RouterLink>
         <div>
             <nav>
-                <ul class=" mx-7
-            relative
-            flex flex-shrink-0 flex-grow-0
-            items-center
-            justify-start
-            gap-20
-            font-space-grotesk
-            text-[24px]
-            font-bold
-            text-white pr-5
-          ">
+                <ul
+                    class=" mx-7 relative flex flex-shrink-0 flex-grow-0 items-center  justify-start gap-20 font-space-grotesk text-[24px] font-bold text-center   text-white pr-5 ">
                     <li>
                         <RouterLink class=" lg:text-lg " to="/projetsco">Projets</RouterLink>
                     </li>
@@ -26,7 +36,10 @@
             </nav>
         </div>
     </div>
+    <!-- Fin du Menu Format MD -->
 </template>
+
+
 <script>
 import Logo from "./icons/IconLogo.vue";
 export default {
@@ -34,7 +47,10 @@ export default {
 }
 </script>
 
+
+
 <style>
+/* Menu md:flex */
 nav ul li {
     position: relative;
 }
@@ -54,4 +70,117 @@ nav ul li a::after {
 nav ul li a:hover::after {
     width: 50%;
 }
+
+/* Fin Menu md:flex */
+
+
+
+/* Menu 3xs:flex */
+.wrapper {
+    position: fixed;
+    top: 0;
+    left: 0px;
+    height: 100%;
+    width: 100%;
+    z-index: 60;
+    background: #F87171;
+    clip-path: circle(25px at calc(100% - 45px) 45px);
+    transition: all 0.3s ease-in-out;
+}
+
+#active:checked~.wrapper {
+    clip-path: circle(75%);
+}
+
+.menu-btn {
+    position: absolute;
+    z-index: 70;
+    right: 3px;
+    top: 20px;
+    height: 50px;
+    width: 50px;
+    line-height: 50px;
+    border-radius: 50%;
+    cursor: pointer;
+    background: #F87171;
+    transition: all 0.3s ease-in-out;
+}
+
+#active:checked~.menu-btn {
+    color: #fff;
+}
+
+#active:checked~.menu-btn i:before {
+    content: "\f00d";
+}
+
+.wrapper ul {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    list-style: none;
+    text-align: center;
+}
+
+.wrapper ul li {
+    margin: 15px 0;
+}
+
+.wrapper ul li a {
+    color: #fff;
+    z-index: 80;
+    text-decoration: none;
+    font-size: 30px;
+    font-weight: 500;
+    padding: 5px 30px;
+    border-radius: 50px;
+    background: #F87171;
+    position: relative;
+    line-height: 50px;
+    transition: all 0.3s ease;
+}
+
+/* .wrapper ul li a:after {
+    position: absolute;
+    z-index: 90;
+    content: "";
+    background: #fff;
+    background: linear-gradient(#F87, #ffffff, #F87171);
+    background: linear-gradient(375deg, #1cc7d0, #2ede98);
+    width: 104%;
+    height: 110%;
+    left: -2%;
+    top: -5%;
+    
+    border-radius: 50px;
+    transform: scaleY(0);
+    z-index: -1;
+    animation: rotate 1.5s linear infinite;
+    transition: transform 0.3s ease;
+} */
+
+/* .wrapper ul li a:hover:after {
+    transform: scaleY(1);
+}
+
+.wrapper ul li a:hover {
+    color: #fff;
+} */
+
+input[type="checkbox"] {
+    display: none;
+
+    @keyframes rotate {
+        0% {
+            filter: hue-rotate(0deg);
+        }
+
+        100% {
+            filter: hue-rotate(360deg);
+        }
+    }
+}
+
+/* Fin menu 3xs:flex */
 </style>
