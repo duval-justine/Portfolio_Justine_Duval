a7e59a9ae90b456809bdaa0686b49a6244175129
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-07 15:45:28 +0100

    Importation de deux Vagues fond

---
+++ src/components/VagueBott.vue
@@ -0,0 +1,8 @@
+<template>
+    <svg width="1440" height="131" viewBox="0 0 1440 131" fill="none" xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none">
+        <path
+            d="M897.5 69.8281C1059.58 124.464 1330.5 61.1614 1440 26.8281V130.828H-2V68.3281C230.833 54.4948 455.5 -79.1719 897.5 69.8281Z"
+            fill="#F87171"></path>
+    </svg>
+</template>
---
+++ src/components/VagueTop.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg width="1440" height="131" viewBox="0 0 1440 131" fill="none" xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none">
+        <path d="M542.5 61C380.425 6.36385 109.5 69.6667 0 104V1.52588e-05H1442V62.5C1209.17 76.3333 984.5 210 542.5 61Z"
+            fill="#F87171"></path>
+    </svg>
+</template>
