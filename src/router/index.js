0621f9e86e9f5363a7efe20abacd1461d440515d
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-08 13:27:31 +0100

    Création de la page SherazadeBNails et du chemin

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import AProposView from '../views/AProposView.vue'
 import CitadelleView from '../views/CitadelleView.vue'
 import Janvier2023View from '../views/Janvier2023View.vue'
 import CharteBavansView from '../views/CharteBavansView.vue'
+import SherazadeBNailsView from '../views/SherazadeBNailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,7 @@ const router = createRouter({
     { path: '/citadelle', name: 'Citadelle', component: CitadelleView },
     { path: '/janvier2023', name: 'Illustration de Janvier 2023', component: Janvier2023View },
     { path: '/chartebavans', name: 'Charte Graphique de Bavans', component: CharteBavansView },
+    { path: '/sherazadebnails', name: 'Affiche Tarifs HomeSalon ShérazadeBNails', component: SherazadeBNailsView },
 
   ]
 })
