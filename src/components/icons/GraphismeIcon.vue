11bfc3975108abb47f2f7ea388fbbb009a7423b5
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-08 13:25:08 +0100

    Ajout d'icon graphisme et photographie

---
+++ src/components/icons/GraphismeIcon.vue
@@ -0,0 +1,33 @@
+<template>
+    <svg width="100" height="92" viewBox="0 0 100 92" fill="none" xmlns="http://www.w3.org/2000/svg"
+        class="w-[100px] h-[90.5px]" preserveAspectRatio="none">
+        <path d="M45 69.5L39.5 82" stroke="#FAFAFA" stroke-width="3"></path>
+        <path d="M55 70L60.5 82" stroke="#FAFAFA" stroke-width="3"></path>
+        <path
+            d="M35.499 82.5C29.499 83.7 30.3324 88.3333 31.499 90.5H68.5C72.9 85.3 68.3333 83 65.5 82.5C57.9997 82 41.499 81.3 35.499 82.5Z"
+            stroke="#FAFAFA" stroke-width="3"></path>
+        <rect x="1.5" y="1.5" width="97" height="59" rx="6.5" stroke="#FAFAFA" stroke-width="3"></rect>
+        <rect x="1.5" y="1.5" width="97" height="68" rx="6.5" stroke="#FAFAFA" stroke-width="3"></rect>
+        <mask id="path-6-inside-1_925_161" fill="white">
+            <rect x="20" y="35" width="12" height="12" rx="2"></rect>
+        </mask>
+        <rect x="20" y="35" width="12" height="12" rx="2" stroke="#FAFAFA" stroke-width="6"
+            mask="url(#path-6-inside-1_925_161)"></rect>
+        <mask id="path-7-inside-2_925_161" fill="white">
+            <rect x="45" y="9" width="12" height="12" rx="2"></rect>
+        </mask>
+        <rect x="45" y="9" width="12" height="12" rx="2" stroke="#FAFAFA" stroke-width="6"
+            mask="url(#path-7-inside-2_925_161)"></rect>
+        <mask id="path-8-inside-3_925_161" fill="white">
+            <rect x="71" y="9" width="12" height="12" rx="2"></rect>
+        </mask>
+        <rect x="71" y="9" width="12" height="12" rx="2" stroke="#FAFAFA" stroke-width="6"
+            mask="url(#path-8-inside-3_925_161)"></rect>
+        <circle cx="19" cy="15" r="3.5" stroke="#FAFAFA" stroke-width="3"></circle>
+        <path d="M26 36.5C26.8333 31.1667 32.1 19.4 46.5 15" stroke="#FAFAFA" stroke-width="3"></path>
+        <path d="M23 15.5H46.5" stroke="#FAFAFA" stroke-width="3"></path>
+        <path d="M55.5 15.5H72.5" stroke="#FAFAFA" stroke-width="3"></path>
+        <path d="M74.5 38L58 26.5L59 48L64.5 40.5L74.5 38Z" stroke="#FAFAFA" stroke-width="3" stroke-linejoin="round">
+        </path>
+    </svg>
+</template>
---
+++ src/components/icons/PhotographieIcon.vue
@@ -0,0 +1,44 @@
+<template>
+    <svg width="100" height="76" viewBox="0 0 100 76" fill="none" xmlns="http://www.w3.org/2000/svg"
+        class="w-[100px] h-[73.5px]" preserveAspectRatio="none">
+        <rect x="1.5" y="11" width="97" height="63" rx="6.5" stroke="#FAFAFA" stroke-width="3"></rect>
+        <line x1="1" y1="25.5" x2="26" y2="25.5" stroke="#FAFAFA" stroke-width="3"></line>
+        <line x1="73" y1="25.5" x2="98" y2="25.5" stroke="#FAFAFA" stroke-width="3"></line>
+        <mask id="path-4-inside-1_926_152" fill="white">
+            <rect x="6" y="59.5" width="16" height="10" rx="2"></rect>
+        </mask>
+        <rect x="6" y="59.5" width="16" height="10" rx="2" stroke="#FAFAFA" stroke-width="5"
+            mask="url(#path-4-inside-1_926_152)"></rect>
+        <mask id="path-5-inside-2_926_152" fill="white">
+            <rect x="6" y="4.5" width="11" height="8" rx="2"></rect>
+        </mask>
+        <rect x="6" y="4.5" width="11" height="8" rx="2" stroke="#FAFAFA" stroke-width="5"
+            mask="url(#path-5-inside-2_926_152)"></rect>
+        <mask id="path-6-inside-3_926_152" fill="white">
+            <rect x="20" y="4.5" width="11" height="8" rx="2"></rect>
+        </mask>
+        <rect x="20" y="4.5" width="11" height="8" rx="2" stroke="#FAFAFA" stroke-width="5"
+            mask="url(#path-6-inside-3_926_152)"></rect>
+        <mask id="path-7-inside-4_926_152" fill="white">
+            <rect x="83" y="4.5" width="11" height="8" rx="2"></rect>
+        </mask>
+        <rect x="83" y="4.5" width="11" height="8" rx="2" stroke="#FAFAFA" stroke-width="5"
+            mask="url(#path-7-inside-4_926_152)"></rect>
+        <mask id="path-8-inside-5_926_152" fill="white">
+            <rect x="41" y="5" width="16" height="7" rx="2"></rect>
+        </mask>
+        <rect x="41" y="5" width="16" height="7" rx="2" stroke="#FAFAFA" stroke-width="5"
+            mask="url(#path-8-inside-5_926_152)"></rect>
+        <path
+            d="M74.5 41.5093C74.5 55.019 63.3337 66.0185 49.5 66.0185C35.6663 66.0185 24.5 55.019 24.5 41.5093C24.5 27.9995 35.6663 17 49.5 17C63.3337 17 74.5 27.9995 74.5 41.5093Z"
+            stroke="#FAFAFA" stroke-width="3"></path>
+        <circle cx="50.5" cy="42" r="19" stroke="#FAFAFA" stroke-width="3"></circle>
+        <path d="M25 36V20.5L39.3535 5.5L40.8384 2H57.1717L58.6566 4.5L74 20.5V36" stroke="#FAFAFA" stroke-width="3"
+            stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M50.5 29.5C54.1667 29.6667 61.6 32.2 62 41" stroke="#FAFAFA" stroke-width="3" stroke-linecap="round"
+            stroke-linejoin="round"></path>
+        <path d="M37.5 43C37.5 47 39.8 54.9 49 54.5" stroke="#FAFAFA" stroke-width="3" stroke-linecap="round"
+            stroke-linejoin="round"></path>
+        <circle cx="62.5" cy="47" r="1.5" fill="#FAFAFA"></circle>
+    </svg>
+</template>
