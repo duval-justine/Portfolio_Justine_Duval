be9f6d853bfb1e424d6818f41ca4befe83db29b9
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-13 18:56:31 +0100

    Ajout image deco Sherazade

--- src/views/SherazadeBNailsView.vue
+++ src/views/SherazadeBNailsView.vue
@@ -3,13 +3,15 @@
     <section>
         <TopPage img_test="/images/Sherazade/Sherazade_Couverture.webp" class="h-[100vh]" />
     </section>
+
+    <img src="../../public/images/Sherazade/Sherazade_DecorationTop.png" class="absolute right-0" alt="">
     <!-- Présentation -->
     <section>
         <div class="flex flex-col justify-center items-center mt-28">
             <h1 class="font-space-grotesk font-semibold text-3xl text-Extended/true-gray/900 text-center">Affiche Tarifs
             </h1>
             <div class=" border-t-[3px] mt-14 w-[404px] border-Extended/true-gray/900"></div>
-            <div class="flex flex-row gap-52 items-start font-karla text-Extended/true-gray/900 text-center mt-14 mb-48">
+            <div class="flex flex-row gap-52 items-start font-karla text-Extended/true-gray/900 text-center mt-14 mb-28">
                 <div class="w-48">
                     <p class="font-extrabold text-base uppercase">Client</p>
                     <p class="font-normal text-2xl">HomeSalon ShérazadeBNails</p>
@@ -37,6 +39,9 @@
             </div>
             <h2 class="font-karla font-bold text-2xl text-Extended/true-gray/900 ">HomeSalon ShérazadeBNails :
             </h2>
+            <!-- Texte -->
+            <div class="flex flex-col gap-3 ml-36 font-karla text-lg font-light text-Extended/true-gray/900 mt-4 mr-72">
+            </div>
         </div>
     </section>
 </template>
