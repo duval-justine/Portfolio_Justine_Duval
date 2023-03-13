aa3f776d6cedde1fccf859673ac1b6e9533084c4
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-13 09:14:03 +0100

    CRBX : l'équipe

--- src/views/CRBXView.vue
+++ src/views/CRBXView.vue
@@ -69,6 +69,29 @@
                 mettant en avant que les NFT ne sont pas une question d’argent mais plutôt une question d’héritage.</p>
         </div>
     </section>
+
+    <!-- L'équipe -->
+    <section>
+        <!-- Titre -->
+        <div class="mt-16 flex flex-row gap-10 items-center">
+            <div class="flex flex-row gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold text-3xl text-Extended/red/900">03</p>
+            </div>
+            <h2 class="font-space-grotesk font-bold text-2xl text-Extended/true-gray/900 ">Notre équipe
+                :
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div class="flex flex-col gap-3 ml-36 font-karla text-base font-light text-Extended/true-gray/900 mt-4 mr-72">
+            <p>• Luca Thomesse : Chef de projet et chargé de communication</p>
+            <p>• Eden Bourezg : Chargé de Design</p>
+            <p>• Justine Duval : Chargé de Design</p>
+            <p>• Quentin Heinis : Chargé de développement</p>
+        </div>
+    </section>
 </template>
 
 <script>
