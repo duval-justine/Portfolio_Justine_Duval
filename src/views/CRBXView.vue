39f8f655e73b0652373d502aae7e15b7a6c9226b
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-13 08:58:06 +0100

    Top Page pour la page CRBX

---
+++ src/views/CRBXView.vue
@@ -0,0 +1,13 @@
+<template>
+    <!-- Top Page -->
+    <section>
+        <TopPage img_test="/images/CRBX/CRBX_Couverture.webp" class="h-[100vh]" />
+    </section>
+</template>
+
+<script>
+import TopPage from "../components/TopPage.vue";
+export default {
+    components: { TopPage }
+}
+</script>
