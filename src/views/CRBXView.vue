fce1f2bb18a3c621a7d16761ed2df2d9d26b8730
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-13 09:13:21 +0100

    CRBX : le commanditaire

--- src/views/CRBXView.vue
+++ src/views/CRBXView.vue
@@ -3,6 +3,7 @@
     <section>
         <TopPage img_test="/images/CRBX/CRBX_Couverture.webp" class="h-[100vh]" />
     </section>
+
     <!-- Présentation -->
     <section>
         <div class="flex flex-col justify-center items-center mt-28">
@@ -23,6 +24,27 @@
                     <p class="font-normal text-2xl">Figma</p>
                 </div>
             </div>
+        </div>
+    </section>
+
+    <!-- Le commanditaire -->
+    <section>
+        <!-- Titre -->
+        <div class="mt-16 flex flex-row gap-10 items-center">
+            <div class="flex flex-row gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold text-3xl text-Extended/red/900">01</p>
+            </div>
+            <h2 class="font-space-grotesk font-bold text-2xl text-Extended/true-gray/900 ">Notre commanditaire :
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div class="flex flex-col gap-3 ml-36 font-karla text-base font-light text-Extended/true-gray/900 mt-4 mr-72">
+            <p>• M. Ayuso est le directeur et le fondateur du Festival OFFF de Barcelona qui est un festival annuel de
+                design, d’art numérique et de créativité qui se déroule à Barcelone en Espagne.</p>
+            <p>• M. Chatonnay est un professeur du département MMI de Montbéliard spécialisé dans le Web.</p>
         </div>
     </section>
 </template>
