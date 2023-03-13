b2566834ba8384ec0c15a59c633608bf2bc63a56
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-13 09:13:44 +0100

    CRBX : la demande

--- src/views/CRBXView.vue
+++ src/views/CRBXView.vue
@@ -47,6 +47,28 @@
             <p>• M. Chatonnay est un professeur du département MMI de Montbéliard spécialisé dans le Web.</p>
         </div>
     </section>
+
+    <!-- Le demande -->
+    <section>
+        <!-- Titre -->
+        <div class="mt-16 flex flex-row gap-10 items-center">
+            <div class="flex flex-row gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold text-3xl text-Extended/red/900">02</p>
+            </div>
+            <h2 class="font-space-grotesk font-bold text-2xl text-Extended/true-gray/900 ">La demande de notre commanditaire
+                :
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div class="flex flex-col gap-3 ml-36 font-karla text-base font-light text-Extended/true-gray/900 mt-4 mr-72">
+            <p>Pour ce projet, nous avions comme mission de créer un générateur de NFT pour le Festival du OFFF de Barcelone
+                qui a lieu en mars 2023. Nous devions réaliser le projet le plus révolutionnaire de tous les temps en
+                mettant en avant que les NFT ne sont pas une question d’argent mais plutôt une question d’héritage.</p>
+        </div>
+    </section>
 </template>
 
 <script>
