7cee5a788d696e867756d79aa99990731a75923a
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-14 18:01:10 +0100

    Chemin Galerie Photos

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import CharteBavansView from '../views/CharteBavansView.vue'
 import CRBXView from '../views/CRBXView.vue'
 import SherazadeBNailsView from '../views/SherazadeBNailsView.vue'
 import Fevrier2023View from '../views/Fevrier2023View.vue'
+import GaleriePhotosView from '../views/GaleriePhotosView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,7 @@ const router = createRouter({
     { path: '/crbx', name: 'Create Box', component: CRBXView },
     { path: '/sherazadebnails', name: 'Affiche pour le HomeSalon Shérazade', component: SherazadeBNailsView },
     { path: '/fevrier2023', name: 'Illustration de Février 2023', component: Fevrier2023View },
+    { path: '/galeriephotos', name: 'Galerie Photos', component: GaleriePhotosView },
 
   ]
 })
