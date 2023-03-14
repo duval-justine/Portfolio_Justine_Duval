c1b43b4d90882640cb021ba35e2a1549d738bac1
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-14 18:02:19 +0100

    Galerie Photos Top Page et Footer

---
+++ src/views/GaleriePhotosView.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- Top Page -->
+    <section>
+        <TopPage img_test="/images/" class="h-[100vh]" />
+    </section>
+
+    <!-- Footer -->
+    <Footerc />
+</template>
+
+<script>
+import TopPage from "../components/TopPage.vue";
+import Footerc from "../components/Footer.vue";
+
+export default {
+    components: { TopPage, Footerc },
+
+}
+</script>
+
