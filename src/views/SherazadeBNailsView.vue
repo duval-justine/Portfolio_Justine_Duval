772ac7c120e979e0adbaec6d5d7c4f16bd94e8c6
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-14 10:57:51 +0100

    Sherazade Responsive sauf Moodboard

--- src/views/SherazadeBNailsView.vue
+++ src/views/SherazadeBNailsView.vue
@@ -4,55 +4,64 @@
         <TopPage img_test="/images/Sherazade/Sherazade_Couverture.webp" class="h-[100vh]" />
     </section>
 
-    <img src="../../public/images/Sherazade/Sherazade_DecorationTop.png" class="absolute right-0" alt="">
+    <img src="../../public/images/Sherazade/Sherazade_DecorationTop.png"
+        class="absolute right-0 3xs:w-[125px] 2xs:w-[180px] xs:w-[200px] sm:w-[250px] md:w-auto h-auto" alt="">
+
     <!-- Présentation -->
     <section>
-        <div class="flex flex-col justify-center items-center mt-28">
-            <h1 class="font-space-grotesk font-semibold text-3xl text-Extended/true-gray/900 text-center">Affiche Tarifs
+        <div class="flex flex-col justify-center items-center 3xs:mt-20  md:mt-28">
+            <h1
+                class="font-space-grotesk font-semibold 3xs:text-2xl  xs:text-2xl md:text-3xl text-Extended/true-gray/900 text-center">
+                Affiche Tarifs
             </h1>
-            <div class=" border-t-[3px] mt-14 w-[404px] border-Extended/true-gray/900"></div>
-            <div class="flex flex-row gap-52 items-start font-karla text-Extended/true-gray/900 text-center mt-14 mb-28">
-                <div class="w-48">
-                    <p class="font-extrabold text-base uppercase">Client</p>
-                    <p class="font-normal text-2xl">HomeSalon ShérazadeBNails</p>
+            <div
+                class="border-t-[3px] 3xs:mt-6 xs:mt-5 sm:mt-8 md:mt-14 3xs:w-[250px] xs:w-[300px] sm:w-[404px] border-Extended/true-gray/900">
+            </div>
+            <div
+                class="flex 3xs:flex-col xs:flex-row 3xs:gap-12  xs:gap-10 sm:gap-40 md:gap-52 3xs:items-center xs:items-start font-karla text-Extended/true-gray/900 text-center 3xs:mt-10 xs:mt-10 md:mt-14 3xs:mb-16 2xs:mb-20 xs:mb-16 sm:mb-20 md:mb-28">
+                <div class="xs:w-48">
+                    <p class="font-extrabold  3xs:text-sm md:text-base uppercase">Client</p>
+                    <p class="font-normal 3xs:text-xl xs:text-lg sm:text-xl md:text-2xl">HomeSalon ShérazadeBNails</p>
                 </div>
                 <div class="w-48">
-                    <p class="font-extrabold text-base uppercase">Date</p>
-                    <p class="font-normal text-2xl">Mars 2023</p>
+                    <p class="font-extrabold 3xs:text-sm md:text-base uppercase">Date</p>
+                    <p class="font-normal 3xs:text-xl xs:text-lg sm:text-xl md:text-2xl">Mars 2023</p>
                 </div>
                 <div class="w-48">
-                    <p class="font-extrabold text-base uppercase">Logiciel</p>
-                    <p class="font-normal text-2xl">Photoshop</p>
+                    <p class="font-extrabold 3xs:text-sm md:text-base uppercase">Logiciel</p>
+                    <p class="font-normal 3xs:text-xl xs:text-lg sm:text-xl md:text-2xl">Photoshop</p>
                 </div>
             </div>
         </div>
     </section>
 
-    <!-- Présentation -->
+    <!-- HomeSalon ShérazadeBNails -->
     <section>
         <!-- Titre -->
-        <div class="mt-16 flex flex-row gap-10 items-center">
-            <div class="flex flex-row gap-4 items-center">
+        <div class="xs:mt-10 sm:mt-14 md:mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
                 <div
                     class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
                 </div>
-                <p class="font-space-grotesk font-bold text-3xl text-Extended/red/900">01</p>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">01</p>
             </div>
-            <h2 class="font-karla font-bold text-2xl text-Extended/true-gray/900 ">HomeSalon ShérazadeBNails :
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">HomeSalon
+                ShérazadeBNails
+                :
             </h2>
         </div>
 
         <!-- Texte -->
-        <div class="flex flex-row gap-24 items-center">
+        <div class="flex 3xs:flex-col sm:flex-row 3xs:gap-5 xs:gap-8 sm:gap-12 md:gap-24 items-center sm:mr-6 md:mr-9">
             <div
-                class="flex flex-col gap-3 ml-36 font-karla text-lg font-light text-Extended/true-gray/900 mt-4 w-[900px] text-justify">
+                class="flex 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-0  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4 sm:w-[700px] md:w-[900px] text-justify">
                 <p>Le Home Salon ShérazadeBNails est un salon de beauté avec une prothésiste ongulaire diplômée et certifiée
                     dans la commune de Bavans qui propose de nombreuses prestations.
                 </p>
             </div>
             <div>
-                <img src="../../public/images/Sherazade/logosherazadebnails.svg" class="w-auto h-44 object-cover mr-9"
-                    alt="">
+                <img src="../../public/images/Sherazade/logosherazadebnails.svg"
+                    class="sm:w-[150px] md:w-[200px] h-auto object-cover " alt="">
             </div>
         </div>
     </section>
@@ -60,19 +69,22 @@
     <!-- Demande de la Cliente -->
     <section>
         <!-- Titre -->
-        <div class="mt-16 flex flex-row gap-10 items-center">
-            <div class="flex flex-row gap-4 items-center">
+        <div class="mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
                 <div
                     class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
                 </div>
-                <p class="font-space-grotesk font-bold text-3xl text-Extended/red/900">02</p>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">02</p>
             </div>
-            <h2 class="font-karla font-bold text-2xl text-Extended/true-gray/900 ">Demande de la cliente :
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">Demande de la
+                cliente
+                :
             </h2>
         </div>
+
         <!-- Texte -->
         <div
-            class="flex flex-col gap-3 ml-36 font-karla text-lg font-light text-Extended/true-gray/900 mt-4 mr-72 text-justify">
+            class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
             <p>En 2022, la cliente m’a contacté pour refaire son affiche tarifaire dans l’esprit du logo. Elle m’a fait part
                 de ses suggestions notamment d’apporter un style orientale à l’affiche ainsi que de mettre en avant 3
                 sections différentes. Au cours de l’année j’ai réalisé des modifications en changeant les tarifs ou encore
@@ -83,17 +95,18 @@
     <!-- Le MoodBoard -->
     <section>
         <!-- Titre -->
-        <div class="mt-16 flex flex-row gap-10 items-center">
-            <div class="flex flex-row gap-4 items-center">
+        <div class="mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
                 <div
                     class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
                 </div>
-                <p class="font-space-grotesk font-bold text-3xl text-Extended/red/900">05</p>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">03</p>
             </div>
-            <h2 class="font-space-grotesk font-bold text-2xl text-Extended/true-gray/900 ">Notre MoodBoard :
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">Mon MoodBoard
+                :
             </h2>
         </div>
-        <div>
+        <div class="3xs:hidden sm:flex">
             <div class="bg-[#FFFDF5]">
                 <div class="cases-carousel my-4">
                     <div class="case-item">
@@ -113,19 +126,20 @@
     <!-- Résultat Final -->
     <section>
         <!-- Titre -->
-        <div class="mt-16 flex flex-row gap-10 items-center">
-            <div class="flex flex-row gap-4 items-center">
+        <div class="mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
                 <div
                     class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
                 </div>
-                <p class="font-space-grotesk font-bold text-3xl text-Extended/red/900">04</p>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">04</p>
             </div>
-            <h2 class="font-karla font-bold text-2xl text-Extended/true-gray/900 ">Résultat Final:
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">Résultat Final
+                :
             </h2>
         </div>
         <!-- Texte -->
         <div
-            class="flex flex-col gap-12 ml-36 font-karla text-lg font-light text-Extended/true-gray/900 mt-4 mr-72 text-justify mb-44">
+            class="flex flex-col gap-12 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify mb-44">
             <p>Voici la dernière affiche que j’ai réalisée qui date de Mars 2023.
                 J’ai donc respecté au maximum les attentes de la cliente en réalisant cette affiche dans un esprit orientale
                 notamment en ajoutant des ornements dorés dans les coins. J’ai également choisi une police de caractère
@@ -136,8 +150,8 @@
 
             </p>
             <div class="flex justify-center">
-                <img src="../../public/images/Sherazade/Sherazade_AfficheTarifs.webp" class="h-auto w-[45%] shadow-2xl"
-                    alt="">
+                <img src="../../public/images/Sherazade/Sherazade_AfficheTarifs.webp"
+                    class="h-auto 3xs:w-[95%] xs:w-[70%] sm:w-[55%] md:w-[45%] shadow-2xl" alt="">
             </div>
         </div>
     </section>
@@ -194,4 +208,5 @@ body,
     height: 100%;
     display: flex;
     flex-wrap: nowrap;
-}</style>
+}
+</style>
