8029260c59958b8ad44d34f98fd6af9efbbd4588
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-14 12:33:13 +0100

    Fevrier2023 Illustration

--- src/views/Fevrier2023View.vue
+++ src/views/Fevrier2023View.vue
@@ -127,7 +127,7 @@
 
         <!-- Desktop -->
         <div class="3xs:hidden sm:flex">
-            <div class="bg-[#FFFDF5]">
+            <div class="bg-[#FFDED9]">
                 <div class="cases-carousel my-4">
                     <div class="case-item">
                         <img src="../../public/images/Fevrier2023/Fevrier2023_Scroll1.webp" class="imgscroll1" alt="">
@@ -138,6 +138,80 @@
                     <div class="case-item">
                         <img src="../../public/images/Fevrier2023/Fevrier2023_Scroll3.webp" class="imgscroll1" alt="">
                     </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Carte Postale -->
+    <section>
+        <!-- Titre -->
+        <div class="xs:mt-10 sm:mt-14 md:mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">04</p>
+            </div>
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">Carte Postale
+                :
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div
+            class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
+            <p>Pour ma carte postale, j’ai réalisé une illustration avec un cadeau qui est ouvert où des ballons en forme de
+                cœur s’envolent de celui-ci. J’ai mis en avant le cadeau avec une couleur rouge et les ballons de différents
+                tons de rose pour rester dans les couleurs que j’avais sélectionné. Pour terminer, j’ai ajouté Happy
+                Valentine’s Day pour faire un rappel de mon thème et du mois de l’illustration.
+            </p>
+            <div class="flex flex-row gap-11 justify-center items-center mt-12">
+                <div>
+                    <img src="../../public/images/Fevrier2023/Fevrier2023_CartePostaleMockup.webp" alt="">
+                </div>
+                <div>
+                    <img src="../../public/images/Fevrier2023/Fevrier2023_CartePostale.webp" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Illustrations & Motion Design -->
+    <section>
+        <!-- Titre -->
+        <div class="xs:mt-10 sm:mt-14 md:mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">05</p>
+            </div>
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">Illustrations
+                & Motion Design
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div
+            class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
+            <p>En ce qui concerne ma frise publiée sur Instagram, j’ai donc réalisé deux autres illustrations au format
+                carré. Tout d’abord, j’ai dessiné une boîte de chocolat en forme de cœur puisque généralement à cette fête
+                nous recevons des chocolats. J’ai donc mis en évidence la boîte en utilisant la couleur rouge ce qui apporte
+                du contraste avec le rose. Pour la deuxième illustration, j’ai réalisé une cassette de musique qui cette
+                fois-ci est constituée de seulement des tons roses.
+            </p>
+            <p>Parmi les illustrations que j’ai faites, j’ai choisi d’animer ma carte postale avec un Motion design où le
+                cadeau s’ouvre et permet au ballon de s’envoler. Pour cela j'ai utilisé le logiciel After Effect.
+            </p>
+            <div class="flex flex-row gap-24 justify-center items-center mt-12">
+                <div>
+                    <img src="../../public/images/Fevrier2023/Fevrier2023_MotionDesign.gif"
+                        class="h-auto w-[390px] shadow-lg" alt="">
+                </div>
+                <div class="flex flex-col gap-24">
+                    <img src="../../public/images/Fevrier2023/Fevrier2023_Illustration1.webp" class="w-[300px] shadow-lg"
+                        alt="">
+                    <img src="../../public/images/Fevrier2023/Fevrier2023_Illustration2.webp" class="w-[300px] shadow-lg"
+                        alt="">
                 </div>
             </div>
         </div>
