c41d34e896706890ab8a5f8b869ec165577cacbb
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-14 17:17:23 +0100

    Ivon Activités Manuelles

---
+++ src/components/icons/ActivitesIcon.vue
@@ -0,0 +1,31 @@
+<template>
+    <svg width="64" height="91" viewBox="0 0 64 91" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-16 h-[88.5px]"
+        preserveAspectRatio="none">
+        <path d="M4.5 51H59.5V82.5C59.5 86.0899 56.5899 89 53 89H11C7.41015 89 4.5 86.0899 4.5 82.5V51Z" stroke="#FAFAFA"
+            stroke-width="3"></path>
+        <path
+            d="M6 42H58C60.4853 42 62.5 44.0147 62.5 46.5C62.5 48.9853 60.4853 51 58 51H6C3.51472 51 1.5 48.9853 1.5 46.5C1.5 44.0147 3.51472 42 6 42Z"
+            stroke="#FAFAFA" stroke-width="3"></path>
+        <mask id="path-3-inside-1_1033_476" fill="white">
+            <path d="M8 2.5H32V37.5H8V2.5Z"></path>
+        </mask>
+        <path
+            d="M8 2.5V0.5H6V2.5H8ZM32 2.5H34V0.5H32V2.5ZM8 4.5H32V0.5H8V4.5ZM30 2.5V37.5H34V2.5H30ZM10 37.5V2.5H6V37.5H10Z"
+            fill="#FAFAFA" mask="url(#path-3-inside-1_1033_476)"></path>
+        <mask id="path-5-inside-2_1033_476" fill="white">
+            <path d="M38 14.5H56V37.5H38V14.5Z"></path>
+        </mask>
+        <path d="M38 14.5V12.5H36V14.5H38ZM38 16.5H56V12.5H38V16.5ZM40 37.5V14.5H36V37.5H40Z" fill="#FAFAFA"
+            mask="url(#path-5-inside-2_1033_476)"></path>
+        <path d="M39 15L46.5 2L55 15" stroke="#FAFAFA" stroke-width="2"></path>
+        <line x1="10" y1="13" x2="20" y2="13" stroke="#FAFAFA" stroke-width="3"></line>
+        <line x1="10" y1="30" x2="20" y2="30" stroke="#FAFAFA" stroke-width="3"></line>
+        <line x1="46.5" y1="37.5" x2="46.5" y2="20.5" stroke="#FAFAFA" stroke-width="3"></line>
+        <line x1="54.5" y1="37.5" x2="54.5" y2="20.5" stroke="#FAFAFA" stroke-width="3"></line>
+        <line x1="51.5" y1="69" x2="51.5" y2="57" stroke="#FAFAFA" stroke-width="3" stroke-linecap="round"></line>
+        <line x1="51.5" y1="78" x2="51.5" y2="74" stroke="#FAFAFA" stroke-width="3" stroke-linecap="round"></line>
+        <line x1="10" y1="36" x2="14" y2="36" stroke="#FAFAFA" stroke-width="3"></line>
+        <line x1="10" y1="24" x2="14" y2="24" stroke="#FAFAFA" stroke-width="3"></line>
+        <line x1="10" y1="19" x2="14" y2="19" stroke="#FAFAFA" stroke-width="3"></line>
+    </svg>
+</template>
