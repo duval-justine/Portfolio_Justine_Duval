2968b8bd05a666798f62152e3f60aaedaaaaf519
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-14 09:43:19 +0100

    Fevrier2023 Top Page

---
+++ src/views/Fevrier2023View.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- Top Page -->
+    <section>
+        <TopPage img_test="/images/Fevrier2023/Fevrier2023_Couverture.webp" class="h-[100vh]" />
+    </section>
+</template>
+
+<script>
+import TopPage from "../components/TopPage.vue";
+
+export default {
+    components: { TopPage },
+
+}
+</script>
+
+
