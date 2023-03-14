bf254b8d751d210ff9834e9a2e60ae0d0c9e603e
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-14 18:24:57 +0100

    Galerie d'image responsive

--- src/views/GaleriePhotosView.vue
+++ src/views/GaleriePhotosView.vue
@@ -4,6 +4,13 @@
         <TopPage img_test="/images/GaleriePhotos/Couverture.webp" class="h-[100vh]" />
     </section>
 
+
+    <section>
+        <ul class="galery">
+            <li><img src="../../public/images/Apropos_Justine.webp" alt=""></li>
+        </ul>
+    </section>
+
     <!-- Footer -->
     <Footerc />
 </template>
@@ -17,4 +24,37 @@ export default {
 
 }
 </script>
+
+<style>
+.galery {
+    max-width: 1100px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2vmin;
+    padding: 0 14px;
+}
+
+.galery::after {
+    content: '';
+    display: block;
+    flex-grow: 10;
+}
+
+.galery li {
+    list-style-type: none;
+    height: 250px;
+    flex-grow: 1;
+}
+
+.galery img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
+
+
+
+
 
