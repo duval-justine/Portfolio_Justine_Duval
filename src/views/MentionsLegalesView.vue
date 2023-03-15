3ff02f6cf94570accd6757ac9e5dfa7a45967eb1
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-15 18:07:47 +0100

    ML Top Page + Footer

---
+++ src/views/MentionsLegalesView.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+        <!-- Component Accueil -->
+        <section>
+            <TopPage img_test="/images/MentionsLegales.webp" class="h-[100vh] bg-zinc-500" />
+        </section>
+
+
+
+        <!-- Footer -->
+        <Footerc />
+    </main>
+</template>
+
+<script>
+import TopPage from "../components/TopPage.vue";
+import Footerc from "../components/Footer.vue";
+
+export default {
+    components: { TopPage, Footerc },
+
+}
+</script>
+
