2a9d1684b4acf1bde05a616bb7f4dd738a3fce69
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-15 21:14:58 +0100

    BC Tous les textes

--- src/views/BoulangerieCoinotView.vue
+++ src/views/BoulangerieCoinotView.vue
@@ -66,6 +66,191 @@
         </div>
     </section>
 
+    <!-- La demande -->
+    <section>
+        <!-- Titre -->
+        <div class="mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">02</p>
+            </div>
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">La demande du
+                client :
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div
+            class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
+            <p>Étant en alternance chez Encre Service à Montbéliard, j’ai l’opportunité de pouvoir réaliser des créations
+                pour des clients. La Boulangerie Pâtisserie du Coinot, nous a contacté au mois de Novembre pour réaliser la
+                carte de fin d’année 2022 de la Boulangerie. Pour cela, la boulangerie nous a transmis la liste des éléments
+                à faire figurer sur la carte et nous a évoqué le fait qu’elle n’avait pas d’attente spécifique en ce qui
+                concerne le design. </p>
+        </div>
+    </section>
+
+    <!-- Le gabarit -->
+    <section>
+        <!-- Titre -->
+        <div class="mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">03</p>
+            </div>
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">Le gabarit :
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div
+            class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
+            <p>Pour ce projet, le client nous a fait part qu’il souhaitait que les cartes de fin d’année soient des
+                dépliants en 3 volets. Pour ce faire, j'ai donc utilisé un gabarit spécifique en x mm. Avec plusieurs
+                repères pour produire un travail de qualité. </p>
+        </div>
+    </section>
+
+    <!-- Les croquis -->
+    <section>
+        <!-- Titre -->
+        <div class="mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">05</p>
+            </div>
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">Les croquis :
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div
+            class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
+            <p>Avant de commencer à réaliser des planches de présentation, j’ai réalisé quelques croquis et j’ai fait
+                quelques recherches pour trouver des éléments graphiques à utiliser pour la carte. J’ai aussi regardé les
+                anciennes versions des années précédentes où j’ai pu constater qu’elles ont été réalisées avec une image en
+                fond et quelques éléments de décoration.Mais la dernière version est sur un autre style graphique beaucoup
+                plus sobre et élégant en utilisant des illustrations. J’ai donc choisi de partir dans ce choix graphique
+                pour faciliter la lisibilité des clients. </p>
+        </div>
+    </section>
+
+    <!-- Les illustrations et les couleurs -->
+    <section>
+        <!-- Titre -->
+        <div class="mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">06</p>
+            </div>
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">Les
+                illustrations et les couleurs :
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div
+            class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
+            <p>Comme vous avez pu le constater juste au dessus dans mon MoodBoard, j’ai choisi d’utilisé 4 couleurs
+                différentes dont deux principales qui sont le noir et le blanc qui permettent d’apporter de l’élégance et un
+                bon contraste. Pour ajouter un peu de douceur, j’ai choisi d’utiliser une couleur dorée qui fait un rappel
+                des fêtes de fin d’année et un marron beige qui peut aussi faire un rappel de cette période de l’année. </p>
+            <p>En ce qui concerne les illustrations, nous avons acheté une planche sur Adobe Stock (). Celle-ci à de
+                nombreux éléments graphiques ce qui est parfait pour l'ambiance que je souhaite donner à la carte des fêtes.
+            </p>
+        </div>
+    </section>
+
+    <!-- Première version -->
+    <section>
+        <!-- Titre -->
+        <div class="mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">07</p>
+            </div>
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">
+                Première version :
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div
+            class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
+            <p>Pour ma première planche, j’ai fait le choix de mettre en arrière plan un fond noir ce qui permet de mettre
+                en avant les autres élément graphique, ajouter un aspect sophistiqué à la carte ainsi que d’apporter un bon
+                contraste avec les autres couleurs pour permettre un bonne lisibilité au client. J’ai mis en avant les
+                textes en utilisant du blanc et du doré.
+            </p>
+            <p>En ce qui concerne les polices de caractère j’ai utilisé Sitka qui est une police de caractère sans-serif
+                moderne qui est élégante avec des formes lisses et arrondies ce qui permet de faciliter la lisibilité, je
+                l’ai principalement utilisée pour les titres. Pour les textes, j’ai choisi la police de caractère Karla qui
+                est aussi une police sans-serif et qui est adaptée pour les design modernes et minimalistes.
+            </p>
+            <p>
+                Au niveau des éléments graphiques, j’en ai mis sur toutes les pages pour créer une cohérence entre les
+                différentes pages. J’ai aussi créé une liaison avec le cerfs et le Logo ce qui permet d’attirer le regard
+                ainsi que cela permet de mettre en avant que la Boulangerie vend des produits pour les fêtes de fin d’année
+                notamment les bûches de Noël.
+            </p>
+        </div>
+    </section>
+
+    <!-- Échange avec le client -->
+    <section>
+        <!-- Titre -->
+        <div class="mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">08</p>
+            </div>
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">Échange avec
+                le client :
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div
+            class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
+            <p>Au cours de ma création, j’ai pu avoir un échange avec le client où j’ai pu présenter ma première planche de
+                création pour avoir un avis. Le client a bien aimé le fait que j'ai mis en avant le logo avec le cerfs. Nous
+                avons eu aussi un échange sur l’élaboration des noms des différentes bûches ainsi que des différents tarifs
+                pour lesquels je n’avais pas. J’ai également ajouté la phrase “Ne pas jeter sur la voie publique” qui est
+                très importante pour tous les documents type flyers sous peine de sanctions ! </p>
+        </div>
+    </section>
+
+    <!-- Le résultat final -->
+    <section>
+        <!-- Titre -->
+        <div class="mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
+            <div class="flex flex-row 3xs:gap-4 items-center">
+                <div
+                    class=" 3xs:border-t-2 2xs:border-t-[3px] 3xs:w-2 2xs:w-[13px] xs:w-[57px] border-Extended/true-gray/900">
+                </div>
+                <p class="font-space-grotesk font-bold 3xs:text-lg 2xs:text-2xl md:text-3xl text-Extended/red/900">09</p>
+            </div>
+            <h2 class="font-karla font-bold 3xs:text-lg 2xs:text-xl md:text-2xl text-Extended/true-gray/900 ">Le résultat
+                final
+                :
+            </h2>
+        </div>
+        <!-- Texte -->
+        <div
+            class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
+            <p>À la suite de cet échange, nous en avons eu quelques autres mais sans vraiment de gros changement, j’ai donc
+                ajouté plus d’éléments graphiques, les tarifs manquants et d’autres éléments de texte. Nous avons aussi
+                réfléchi au niveau des couleurs et nous avons donc créer deux versions une avec un fond marron-beige et une
+                autre avec un fond noir. Nous avons réalisé une impression de plusieurs exemplaires qui ont pu être
+                distribués dans la boulangerie du Coinot à Montbéliard lors de la période du marché de Noël. </p>
+        </div>
+    </section>
 
     <!-- Footer -->
     <Footerc />
