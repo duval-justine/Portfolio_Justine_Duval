61e9993fc9d051185521666e8384e68a706adcc8
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-15 21:00:16 +0100

    BC Top Page + Footer

---
+++ src/views/BoulangerieCoinotView.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- Top Page -->
+    <section>
+        <TopPage img_test="/images/BoulangerieCoinot/BoulangerieCoinot_Couverture.webp" class="h-[100vh]" />
+    </section>
+
+    <!-- Footer -->
+    <Footerc />
+</template>
+
+<script>
+
+import TopPage from "../components/TopPage.vue";
+import Footerc from "../components/Footer.vue";
+
+export default {
+    components: { TopPage, Footerc },
+
+}
+</script>
+
