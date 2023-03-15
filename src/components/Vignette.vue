339add648e052312e71bb456ee2a08173b9bc6d7
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-15 15:54:46 +0100

    Refonte du Design Vignette Ordi

--- src/components/Vignette.vue
+++ src/components/Vignette.vue
@@ -1,28 +1,5 @@
 <template>
     <!-- Vignette 3XS to XS -->
-    <!-- <div class=" flex sm:hidden containervi group flex-col items-center">
-        <RouterLink :to="lien">
-            <div class="relative">
-                <img class="w-full h-auto containervi" :src="image" alt="Image">
-                <div
-                    class="absolute bottom-0 h-2 flex flex-row gap-1 justify-center items-center px-4 py-10 bg-black opacity-60">
-                    <div class="text-left opacity-100">
-                        <h3 class="text-xs font-space-grotesk font-bold text-Extended/red/900 mb-2">{{ titre }}
-                        </h3>
-                        <p class="text-[10px] font-space-grotesk font-normal text-Extended/red/50 ">{{ date }}
-                        </p>
-                    </div>
-                    <div class="flex items-center justify-center ">
-                        <button
-                            class="bg-Extended/red/900 h-8 w-20  text-Extended/red/50 font-bold text-xs py-2 px-4 rounded ">Lire
-                            plus</button>
-
-                    </div>
-                </div>
-            </div>
-        </RouterLink>
-    </div> -->
-
     <div class="sm:hidden m-5 w-[313px] h-[370px] relative">
         <img class="rounded-[30px] absolute w-full h-[313px] object-cover shadow-xl " :src="image" alt="">
         <div
@@ -53,6 +30,23 @@
         <div class="relative">
             <img class="w-full h-auto containervi" :src="image" alt="Image">
             <div
+                class="absolute  inset-0 bg-gradient-to-b from-transparent to-Extended/true-gray/900 opacity-0 hover:opacity-95 duration-300">
+                <div class="text-left mx-5 mt-40 opacity-100">
+                    <h3 class="text-lg font-space-grotesk font-bold text-Extended/red/50 mb-2">{{ titrexs }}
+                    </h3>
+                    <p class="text-xs font-space-grotesk font-normal text-Extended/red/50 mt-2">{{ texte }}
+                    </p>
+                </div>
+                <div class="flex opacity-100 items-center justify-end mr-5 mt-2 ">
+                    <RouterLink
+                        class="bg-Extended/red/50 hover:bg-Extended/red/900 h-10 w-24 text-Extended/red/900 hover:text-Extended/red/50 font-bold py-2 px-4 rounded "
+                        :to="lien">
+                        Lire plus
+                    </RouterLink>
+                </div>
+            </div>
+
+            <!-- <div
                 class="absolute containervi inset-0 flex justify-center items-center px-4 py-8 bg-black opacity-0 hover:opacity-80 transition-opacity">
                 <div class="text-left -mt-16 opacity-100">
                     <h3 class="text-lg font-space-grotesk font-bold text-Extended/red/900 mb-2">{{ titre }}
@@ -69,7 +63,7 @@
                         Lire plus
                     </RouterLink>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
