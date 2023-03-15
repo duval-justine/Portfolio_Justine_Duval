ccc04897c7a47cd5ad01d91565095a58ba37b4c7
duval-justine <95071327+duval-justine@users.noreply.github.com>
2023-03-15 21:55:09 +0100

    BC Images Harmonie + Gabarit

--- src/views/BoulangerieCoinotView.vue
+++ src/views/BoulangerieCoinotView.vue
@@ -111,6 +111,20 @@
                 dépliants en 3 volets. Pour ce faire, j'ai donc utilisé un gabarit spécifique en x mm. Avec plusieurs
                 repères pour produire un travail de qualité. </p>
         </div>
+        <div class="flex justify-center">
+            <img src="../../public/images/BoulangerieCoinot/BoulangerieCoinot_Gabarit.webp" class="w-[70%] mt-5" alt="">
+        </div>
+        <div
+            class="flex 3xs:flex-col 3xs:text-center xs:text-left xs:flex-row 3xs:text-xs xs:text-sm sm:text-base 3xs:gap-2 xs:gap-6 sm:gap-8 md:gap-10 font-karla font-light justify-center mt-2">
+            <p>Repère Vert = Fond Perdu
+            </p>
+            <p>
+                Repère Rouge = Taille du dépliant
+            </p>
+            <p>
+                Repère Bleu = Marge à respecter
+            </p>
+        </div>
     </section>
 
     <!-- Les croquis -->
@@ -127,6 +141,25 @@
             </h2>
         </div>
         <!-- Texte -->
+        <!-- <div class="flex 3xs:flex-col sm:flex-row 3xs:gap-5 xs:gap-8 sm:gap-12 md:gap-24 items-center sm:mr-6 md:mr-9">
+            <div
+                class="flex 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-0  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4 sm:w-[700px] md:w-[900px] text-justify">
+                <p>Avant de commencer à réaliser des planches de présentation, j’ai réalisé quelques croquis et j’ai fait
+                    quelques recherches pour trouver des éléments graphiques à utiliser pour la carte. J’ai aussi regardé
+                    les
+                    anciennes versions des années précédentes où j’ai pu constater qu’elles ont été réalisées avec une image
+                    en
+                    fond et quelques éléments de décoration.Mais la dernière version est sur un autre style graphique
+                    beaucoup
+                    plus sobre et élégant en utilisant des illustrations. J’ai donc choisi de partir dans ce choix graphique
+                    pour faciliter la lisibilité des clients.
+                </p>
+            </div>
+            <div>
+                <img src="../../public/images/BoulangerieCoinot/BoulangeriCoinot_Croquis.webp"
+                    class="3xs:w-64 sm:w-[150px] md:w-[300px] h-auto object-cover " alt="">
+            </div>
+        </div> -->
         <div
             class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
             <p>Avant de commencer à réaliser des planches de présentation, j’ai réalisé quelques croquis et j’ai fait
@@ -153,7 +186,26 @@
             </h2>
         </div>
         <!-- Texte -->
-        <div
+        <div class="flex 3xs:flex-col sm:flex-row 3xs:gap-5 xs:gap-8 sm:gap-12 md:gap-24 items-center sm:mr-6 md:mr-9">
+            <div
+                class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-0  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4 sm:w-[700px] md:w-[900px] text-justify">
+                <p>Comme vous avez pu le constater juste au dessus dans mon MoodBoard, j’ai choisi d’utilisé 4 couleurs
+                    différentes dont deux principales qui sont le noir et le blanc qui permettent d’apporter de l’élégance
+                    et un bon contraste. Pour ajouter un peu de douceur, j’ai choisi d’utiliser une couleur dorée qui fait
+                    un rappel
+                    des fêtes de fin d’année et un marron beige qui peut aussi faire un rappel de cette période de l’année.
+                </p>
+                <p>En ce qui concerne les illustrations, nous avons acheté une planche sur Adobe Stock (). Celle-ci à de
+                    nombreux éléments graphiques ce qui est parfait pour l'ambiance que je souhaite donner à la carte des
+                    fêtes.
+                </p>
+            </div>
+            <div>
+                <img src="../../public/images/BoulangerieCoinot/BoulangerieCoinot_HarmonieColoree.webp"
+                    class="3xs:w-64 sm:w-[150px] md:w-[300px] h-auto object-cover " alt="">
+            </div>
+        </div>
+        <!-- <div
             class="flex flex-col gap-3 3xs:mx-[50px] 2xs:mx-[70px] xs:mx-[70px] sm:mr-32  sm:ml-[123px] md:ml-36 font-karla 3xs:text-sm xs:text-base md:text-lg font-light text-Extended/true-gray/900 mt-4  text-justify">
             <p>Comme vous avez pu le constater juste au dessus dans mon MoodBoard, j’ai choisi d’utilisé 4 couleurs
                 différentes dont deux principales qui sont le noir et le blanc qui permettent d’apporter de l’élégance et un
@@ -162,7 +214,7 @@
             <p>En ce qui concerne les illustrations, nous avons acheté une planche sur Adobe Stock (). Celle-ci à de
                 nombreux éléments graphiques ce qui est parfait pour l'ambiance que je souhaite donner à la carte des fêtes.
             </p>
-        </div>
+        </div> -->
     </section>
 
     <!-- Première version -->
@@ -199,6 +251,12 @@
                 notamment les bûches de Noël.
             </p>
         </div>
+        <div class="flex 3xs:flex-col xs:flex-row 3xs:gap-3 xs:gap-6 justify-center mt-14 items-center">
+            <img src="../../public/images/BoulangerieCoinot/BoulangerieCoinot_V1R.webp" class="3xs:w-[80%] xs:w-[40%]"
+                alt="">
+            <img src="../../public/images/BoulangerieCoinot/BoulangerieCoinot_V1V.webp" class="3xs:w-[80%] xs:w-[40%]"
+                alt="">
+        </div>
     </section>
 
     <!-- Échange avec le client -->
@@ -227,7 +285,7 @@
     </section>
 
     <!-- Le résultat final -->
-    <section>
+    <section class="mb-44">
         <!-- Titre -->
         <div class="mt-16 flex flex-row 3xs:gap-2 2xs:gap-4 xs:gap-6 md:gap-10 items-center">
             <div class="flex flex-row 3xs:gap-4 items-center">
@@ -249,6 +307,18 @@
                 réfléchi au niveau des couleurs et nous avons donc créer deux versions une avec un fond marron-beige et une
                 autre avec un fond noir. Nous avons réalisé une impression de plusieurs exemplaires qui ont pu être
                 distribués dans la boulangerie du Coinot à Montbéliard lors de la période du marché de Noël. </p>
+        </div>
+        <div class="flex 3xs:flex-col xs:flex-row 3xs:gap-3 xs:gap-6 justify-center mt-14 items-center">
+            <img src="../../public/images/BoulangerieCoinot/BoulangerieCoinot_V2R.webp" class="3xs:w-[80%] xs:w-[40%]"
+                alt="">
+            <img src="../../public/images/BoulangerieCoinot/BoulangerieCoinot_V2V.webp" class="3xs:w-[80%] xs:w-[40%]"
+                alt="">
+        </div>
+        <div class="flex 3xs:flex-col xs:flex-row 3xs:gap-3 xs:gap-6 justify-center xs:mt-6 items-center">
+            <img src="../../public/images/BoulangerieCoinot/BoulangerieCoinot_V3R.webp" class="3xs:w-[80%] xs:w-[40%]"
+                alt="">
+            <img src="../../public/images/BoulangerieCoinot/BoulangerieCoinot_V3V.webp" class="3xs:w-[80%] xs:w-[40%]"
+                alt="">
         </div>
     </section>
 
